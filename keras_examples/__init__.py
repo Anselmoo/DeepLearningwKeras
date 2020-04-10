982bf7c80c8ae429b5786e66f804715c752f0bf4
Hahn Anselm <hahans@prestagroup.com>
2020-04-10 10:56:42 +0200

    Add requirements.txt

---
+++ keras_examples/__init__.py
@@ -0,0 +1,13 @@
+import tqdm
+import numpy
+import matplotlib
+import keras
+import tensorflow
+
+
+
+print("tqdm=={}".format(tqdm.__version__))
+print("numpy=={}".format(numpy.__version__))
+print("matplotlib=={}".format(matplotlib.__version__))
+print("keras=={}".format(keras.__version__))
+print("tensorflow=={}".format(tensorflow.__version__))
